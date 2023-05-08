f29823a12498a0846138286cdb0d5bb7c93b56be
Sneka <snekasri1427@gmail.com>
2023-05-08 13:35:44 +0000

    Switch  case

---
+++ Switch.php
@@ -0,0 +1,17 @@
+<?php
+$favcar = "BMW";
+
+switch ($favcar) {
+  case "BMW":
+    echo "Your car is BMW!";
+    break;
+  case "Audi":
+    echo "Your car is Audi!";
+    break;
+  case "benz":
+    echo "Your car is benz!";
+    break;
+  default:
+    echo "Your car is neither BMW, Audi, nor benz!";
+}
+?>
